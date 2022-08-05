ae75075497147f8fe6b42ab66fb90dda1e5a71dc
HACK-WU <2931580128@qq.com>
2022-08-05 17:20:58 +0800

    DNS图形化管理工具

--- dns_manager.sh
+++ dns_manager.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/bash
+########################################
+# Author:hackwu
+# time:2022年08月5日 星期日 17时28分45秒
+# filename:dns_manager.sh
+# Script description:
+#	此脚本用户配置dns服务：
+#		-可以进行通用的dns配置
+#		-也可以进行智能dns分离解析配置
+#		-还可以进行主从服务器配。
+#	操作简单，只需要根据提示输入域名已经需要解析的ip地址，即可完成。
+########################################
 
 set -u
 
 named_conf="/etc/named.conf"
 named_rfc_zones="/etc/named.rfc1912.zones"
+named_lan_zones="/etc/lan.zones"  ; > $named_lan_zones
+named_wan_zones="/etc/wan.zones"  ; > $named_wan_zones
 var_named_path="/var/named/"
 
 if [ !  -f "$named_conf".bak ];then
@@ -78,6 +91,8 @@ view wan {
 ############################ named_zones	###################################33
 domain=null	#域名
 A_PTR=null
+lan_wan=null
+
 function zones { 	
 	#参数： 
 	#	$1,置为A后者PTR，表明是反向解析还是正向解析
@@ -88,6 +103,8 @@ function zones {
 	
 	domain="$2"
 	local file="$domain"zone
+	[ "$lan_wan" != "null"  ] && file=$lan_wan/"$file"
+
 	A_PTR="A"
 	domain2=null
 	if [ "$1" == "PTR"  ];then
@@ -98,11 +115,13 @@ function zones {
 	local allow="allow-update { none; };"
 	
 	set +u
-	if [ -z $4 ];then 
+	if [[ -z $4 || "$4" == "master" ]];then 
 		local type1="master"
 	else
-		local type1=$4	#设置type的默认值为master
+		local type1=$4	
 		allow="masters { $5; };"
+		file=slaves/"$file"
+		rm -fr "$var_named_path/slaves/"
 	fi
 	set -u	
 	
@@ -148,7 +167,7 @@ function var_zone {
 		wan)    local lan_wan=wan ;;
 		master) local master_slave=master ;;
 		slave)	local master_slave=slave ;;
-		*)	echo -e "\033[31m参数错误\033[0m" ;;
+		*)	echo -e "\033[31m参数错误\033[0m" ;exit  ;;
 		esac
 	
 		[ -z $lan_wan ] && local lan_wan=no
@@ -182,6 +201,13 @@ function var_zone {
 		echo -e "\033[33m目录授权完毕\033[0m"
 		ls -l $dir_path -d
 	fi
+	
+	set +u
+	if [ "$3" == "slave"  ];then
+		return 0
+	fi	
+	set -u
+	
 	touch $file_path ;
 	chown :named $file_path
 	echo -e "\033[33mzone文件授权完毕\033[0m"
@@ -190,7 +216,9 @@ function var_zone {
 }
 
 function input_zone {	
-	#参数： $@  里面封装了多对参数，每队参数由一个三级域名和一个ip组成
+	#参数： 
+	#	正向解析：$@  里面封装了多对参数，每对参数由一个三级域名和一个ip组成
+	#	反向机械：$@  里面封装了多对参数，每对参数由一个主机号和一个完整的域名组成
 
 	local dom=$domain
 	
@@ -228,36 +256,10 @@ local str="\$TTL 1D
 
 			echo   $(echo "$str"| sed -e "s/dom/$third_domain/g;s/ip/$ip/g ") >> $file_path
 		fi
-		let num++
-		
+		let num++	
 	done
-	
-
-
 }
 
-#conf
-#zones A  hackwu.cn.  $named_rfc_zones
-#var_zone $domain
-#input_zone www	192.168.23.10  hello 192.168.20.13
-
-#conf 
-#zones PTR  192.168.20. $named_rfc_zones
-#var_zone $domain
-#input_zone 12 www.hackwu.cn.  45 hello.hackwu.cn.
-
-#conf
-#zones A  hackwu.cn.  $named_rfc_zones slave 192.168.23.12
-#var_zone $domain wan slave
-#input_zone www	192.168.23.10  hello 192.168.20.13
-
-#conf
-#view  192.168.23.0/24    any
-#zones A  hackwu.cn.  $named_rfc_zones 
-#var_zone $domain lan 
-#input_zone www	192.168.23.10  hello 192.168.20.13
-#var_zone $domain wan
-#input_zone www 192.168.10.10 hello 192.168.20.20
 
 function start {
 	if systemctl status named &>/dev/null ;then
@@ -268,35 +270,116 @@ function start {
 	echo "named 服务启动成功！！"
 }
 
-function A {
+function A { 
+	# 参数：均不可以省略。
+	#	$1: 值为：lan/wan/no
+	#	$2：值为：master/slave  
+	#	$3: 值为：master_ip/  当$2==slave时，需要填写master的ip,当$2==master时，此参数省略
+
+	local title="通用域名解析设置"
 	local dom=$(whiptail --title "二级域名设置" --inputbox "请输入二级域名，比如：hackwu.cn." 10 60  3>&1 1>&2 2>&3)
 	[[ "$dom" =~ ^[a-zA-Z0-9]+\.[a-z]+$  ]]&& dom=$dom.
-	[[ "$dom" =~ ^[a-zA-Z0-9]+\.[a-z]+\.$  ]] || echo "域名格式错误！"
-	zones A $dom $named_rfc_zones
-	var_zone $domain
-	local thrid_dom=$(whiptail --title "三级域名解析设置" --inputbox "请输入三级域名，以及对应的ip，比如:wwww 192.168.23.10 ,可以有多组" 10 60  3>&1 1>&2 2>&3)	
-	input_zone $thrid_dom	
+	[[ "$dom" =~ ^[a-zA-Z0-9]+\.[a-z]+\.$  ]] || {
+					echo -e "\033[31m域名格式错误！\033[0m";
+					exit
+					}
+	
+	
+	if [ "$1" != "no" ] ;then
+		set +u
+		lan_wan=lan
+		zones A $dom $named_lan_zones $2 $3
+		set +u 
+		lan_wan=wan
+		zones A $dom $named_wan_zones $2 $3
+	else
+		set +u	
+		zones A $dom $named_rfc_zones $2 $3  #master/slave:$2  #null/ip:$3
+		set -u
+	fi
+	
+	local lan_wan=$1		#lan/wan/no:$1 
+	if [ "$1" != "no" ];then
+		lan_wan="lan"
+		title="局域网域名解析设置"			
+		local ip=$(whiptail --title "分离解析，网段设置" --inputbox "请输入局域网网段和广域网网段(建议使用any)，比如:192.168.23.0/24 any ." 10 80  3>&1 1>&2 2>&3)
+		view $ip		
+	fi
+		
+	var_zone $domain $lan_wan $2   #	master/slave:$2
+	local thrid_dom=$(whiptail --title "$title" --inputbox "请输入三级域名，以及对应的ip，比如:wwww 192.168.23.10 ,可以有多组" 10 80  3>&1 1>&2 2>&3)	
+	[ "$2" != "slave"  ]&& 	input_zone $thrid_dom
+	
+	if [ "$1" != "no" ];then
+		lan_wan="wan"
+		title="广域网域名解析设置"
+		var_zone $domain $lan_wan $2   #	master/slave:$2
+		local thrid_dom=$(whiptail --title "$title" --inputbox "请输入三级域名，以及对应的ip，比如:wwww 192.168.23.10 ,可以有多组" 10 80  3>&1 1>&2 2>&3)	
+		
+		[ "$2" != "slave"  ]&&	input_zone $thrid_dom
+	fi
+		
 	echo  -e  "\033[33m正向解析配置完成\033[0m"
 	whiptail --title "配置成功" --msgbox "正向解析配置完成" 10 60	
 	
 }
 
-function PTR {
-        local ip=$(whiptail --title "ip网段设置" --inputbox "请输入一个ip网段，比如：192.168.23." 10 60  3>&1 1>&2 2>&3)
-        [[ "$ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+$  ]]&& ip=$ip.
-        [[ "$ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.$  ]] || echo "ip网段格式错误！"
 
-        zones PTR $ip  $named_rfc_zones
-        var_zone $domain
-        local host_dom=$(whiptail --title "反向解析配置" --inputbox "请输入网络号和完整域名，比如:12 www.hackwu.cn. ,可以有多组" 10 60  3>&1 1>&2 2>&3)
-        input_zone $host_dom
+function PTR {
+	# 参数：均不可以省略。
+	#	$1: 值为：lan/wan/no
+	#	$2：值为：master/slave  
+	#	$3: 值为：master_ip/null  当$2==slave时，需要填写master的ip
+ 
+	local title="通用域名解析设置"
+       local ip=$(whiptail --title "ip网段设置" --inputbox "请输入一个ip网段，比如：192.168.23." 10 60  3>&1 1>&2 2>&3)
+        [[ "$ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+$  ]]&& ip=$ip.
+        [[ "$ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.$  ]] || {
+					echo "ip网段格式错误！";
+					exit
+					}
+	
+	if [ "$1" != "no" ] ;then
+                set +u
+                lan_wan=lan
+                zones PTR $ip $named_lan_zones $2 $3
+                set +u 
+                lan_wan=wan
+                zones PTR $ip $named_wan_zones $2 $3
+        else 
+                set +u  
+                zones PTR $ip $named_rfc_zones $2 $3  #master/slave:$2  #null/ip:$3
+                set -u
+        fi
+	
+	local lan_wan=$1
+	if [ "$1" != "no" ];then
+                lan_wan="lan"
+                title="局域网反向解析设置"
+                local ip=$(whiptail --title "分离解析，网段设置" --inputbox "请输入局域网网段和广域网网段(建议使用any)，比如:192.168.23.0/24 any ." 10 80  3>&1 1>&2 2>&3)
+                view $ip
+        fi
+
+        var_zone $domain $lan_wan $2  		#         master/slave:$2
+        local host_dom=$(whiptail --title "$title" --inputbox "请输入网络号和完整域名，比如:12 www.hackwu.cn. ,可以有多组" 10 60  3>&1 1>&2 2>&3)
+       [ "$2" != "slave"  ]&&  input_zone $host_dom
+
+	 if [ "$1" != "no" ];then
+                lan_wan="wan"
+                title="广域网反向解析设置"
+                var_zone $domain $lan_wan $2   #        master/slave:$2
+                local host_dom=$(whiptail --title "$title" --inputbox "请输入网络号和完整域名，比如:12 www.hackwu.cn.,可以有多组" 10 80  3>&1 1>&2 2>&3)
+	[ "$2" != "slave"  ]&& input_zone $host_dom
+        fi
+
 	echo  -e  "\033[33m反向解析配置完成\033[0m"
 	whiptail --title "配置成功" --msgbox "反向解析配置完成" 10 60	
 
 }
 
+
 function dns_common {
-	echo "通用配置"	
+set +u
 while  :
 do
     local OPTION=$(whiptail --title "通用配置" --menu "YES: 确定，NO: 保存退出" 15 60 2\
@@ -305,37 +388,74 @@ do
      
     exitstatus=$?
     if [ $exitstatus  -eq  0 ]; then
-	conf
 	case $OPTION in
-	1)A   ;;
-	2)PTR ;;
+	1)A $1 $2 $3  ;;
+	2)PTR $1 $2 $3  ;;
 	*) break ;;
 	esac
     else
 	break
     fi
 done
+set -u
 		
 }
 
+function allow-transfer {	#主从配置
+	local num=$(grep "allow-query" -n "$named_conf"| cut -d ":" -f 1)
+	local ip=$(whiptail --title "从服务器地址配置" --inputbox "请输入从服务器的IP地址" 10 60  3>&1 1>&2 2>&3)		
+	sed -i  "$num a  allow-transfer { $ip; }; "  $named_conf
+	echo -e "\033[33m从服务器IP配置完毕：$ip \033[0m"
+}
 
-
+function slave {
+	local OPTION=$(whiptail --title "DNS主从服务管理" --menu "请选择：" 15 80 3 \
+            "1" "本机是主服务器 (在主服务器上使用)" \
+            "2" "从服务器通用配置 (在从服务器上使用)" \
+	    "3" "从服务器分离解析配置 (在从服务器上使用) " 3>&1 1>&2 2>&3)	
+	
+	case $OPTION in 
+	1)allow-transfer ; return 0 ;;
+	2)
+		local master_ip=$(whiptail --title "主服务器地址配置" --inputbox "请输入主服务器的IP地址" 10 60  3>&1 1>&2 2>&3)
+		dns_common no slave $master_ip		 
+		echo -e "\033[33m主服务器ip： $ip \033[0m"
+	   ;;
+	3)
+		local master_ip=$(whiptail --title "主服务器地址配置" --inputbox "请输入主服务器的IP地址" 10 60  3>&1 1>&2 2>&3)
+		dns_common lan slave $master_ip		 
+		echo -e "\033[33m主服务器ip： $ip \033[0m"
+	   ;;
+	*) echo "选项错误"; return 1;;
+	esac
+	echo -e "\033[33m从服务器配置完毕！！\033[0m"
+	
+}
 
 function man {
-	OPTION=$(whiptail --title "DNS服务管理" --menu "请选择以下功能：" 15 60 3 \
+	conf
+while :
+do
+	local OPTION=$(whiptail --title "DNS服务管理" --menu "YES:确定，NO：保存退出。" 15 60 3 \
 	    "1" "DNS通用配置" \
 	    "2" "DNS分离解析配置" \
 	    "3" "DNS主从服务器配置" 3>&1 1>&2 2>&3)
      
 	    exitstatus=$?
 	    if [ $exitstatus  -eq  0 ]; then
-	        echo "Your favorite programming language is" $OPTION
+		case $OPTION in 
+		1) dns_common no master   ;;
+		2) dns_common lan master  ;;
+		3) slave ;;
+		*) break ;; 
+		esac
 	    else
-	        echo "You chose Cancel."
+		break
 	    fi
+done
 }
 
-dns_common
+man
 start
 echo "finshed"
 
